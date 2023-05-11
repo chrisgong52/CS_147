d3d52c073e58621d8c29ad473c7014750a9ac122
gshanna <gshanna@uci.edu>
2023-05-11 13:49:51 -0700

    lab 3 part 1 working screen switch with command

---
+++ Lab 3/src/main.cpp
@@ -0,0 +1,61 @@
+#include <Arduino.h>
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#include <TFT_eSPI.h>
+
+TFT_eSPI tft = TFT_eSPI();
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *pCharacteristic) {
+  std::string value = pCharacteristic->getValue();
+  if (value.length() > 0) {
+    if (value.compare("on") == 0)
+    {
+      tft.fillScreen(TFT_WHITE);
+      Serial.print("ENTERED");
+    }
+    else 
+    {
+      Serial.print("Off");
+      tft.fillScreen(TFT_BLACK);
+    }
+    Serial.println("*********");
+    Serial.print("New value: ");
+    for (int i = 0; i < value.length(); i++)
+    Serial.print(value[i]);
+    Serial.println();
+    Serial.println("*********");
+  }
+}
+};
+void setup() {
+  Serial.begin(9600);
+  tft.begin();
+  Serial.println("1- Download and install an BLE scanner app in your phone");
+  Serial.println("2- Scan for BLE devices in the app");
+  Serial.println("3- Connect to MyESP32");
+  Serial.println("4- Go to CUSTOM CHARACTERISTIC in CUSTOM SERVICE and write something");
+  Serial.println("5- See the magic =)");
+  BLEDevice::init("MyESP32");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic
+  (
+    CHARACTERISTIC_UUID,
+    BLECharacteristic::PROPERTY_READ |
+    BLECharacteristic::PROPERTY_WRITE
+  );
+  pCharacteristic->setCallbacks(new MyCallbacks());
+  pCharacteristic->setValue("Hello World");
+  pService->start();
+  BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  pAdvertising->start();
+}
+  
+void loop() {
+  delay(2000);
+}
